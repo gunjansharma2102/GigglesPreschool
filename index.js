825a1703acc63429f7e6fe9e770c70ba702e23ad
Muktika <poddar.muktika@globo-tek.com>
2020-04-05 01:09:22 -0500

    changes- added flow to node

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ var http = require('http');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "myusername",
+  password: "mypassword"
+});
 
-// app.get('/test', function(req, res) {
-// 		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-// });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+var login=[];
+app.get('/login', function(req, res) {
+	console.log(req.query);
+	login.username = req.query.username;
+	login.password = req.query.password;
+	
+	//check in database
+	//if response has 1 row
+
+	// res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+
+	//else if response has no row
+	// res.send(JSON.stringify({"status": 200, "error": null, "response": "ERROR"}));
+
+	//else if error occurs
+	// res.send(JSON.stringify({"status": 404, "error": error, "response": "ERROR"}));
+});
 
--- public/main-controller.js
+++ public/main-controller.js
@@ -1,22 +1,60 @@
+
 angular.module('app')
 .controller('MainController', ['$scope','$http','$state', function ($scope, $http, $state) {
-	$scope.results=[];
-  $scope.button1 = false;
-  $http({
-        method: 'GET',
-        url: '/test'
-      }).then(function successCallback(response) {
-          $scope.results = response.data;
-      }, function errorCallback(error) {
-        console.log(error);
-      });
 
-  $scope.button12 = function(button1){
-    if(button1 == true){
-      button1 = false;
-    }else{
-      button1 = true;
-    }
+  //Login page controller code
+  $scope.username = "";
+  $scope.password = "";
+
+  $scope.loginSubmit = function(){
+    
+    //Check by posting whether username and password exists.
+    $http({
+            method: 'GET',
+            url: '/login',
+            params: {"username":$scope.username,"password":$scope.password}
+          }).then(function successCallback(response) {
+              if(response.data == "Error"){
+                //alert("Some error occurred. Try again.");
+                //Redirect with error back to lgin
+              }else{
+                //alert("Process completed! Check result.");
+                // $scope.showResult=1;
+                // $scope.child = response.data;
+                //Redirect to dashboard
+              }
+          }, function errorCallback(error) {
+            console.log(error);
+            //Some error occurred page - 404
+          });
   } 
+
+  //Signup page controller code
+
+  //Add fields - $scope.
+
+  $scope.signupSubmit = function(){
+    
+    //Check by posting whether username and password exists.
+    $http({
+            method: 'GET',
+            url: '/signup',
+            // params: {"username":$scope.username,"password":$scope.password}
+          }).then(function successCallback(response) {
+              if(response.data == "Error"){
+                //alert("Some error occurred. Try again.");
+                //Redirect with error back to lgin
+              }else{
+                //alert("Process completed! Check result.");
+                // $scope.showResult=1;
+                // $scope.child = response.data;
+                //Redirect to Login
+              }
+          }, function errorCallback(error) {
+            console.log(error);
+            //Some error occurred page - 404
+          });
+  } 
+  
 
 }]);
